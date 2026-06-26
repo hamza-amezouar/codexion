e8e5f78c578b6aecdc2c3cfb7250d9b68f489ed1
Hamza Amezouar <hamezoua@c2r1p4.1337.ma>
2026-06-26 21:04:05 +0100

    I added threads in mini heap

--- simulation.c
+++ simulation.c
@@ -6,7 +6,7 @@
 /*   By: hamezoua <amouzwarh+1@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/22 14:17:13 by username          #+#    #+#             */
-/*   Updated: 2026/06/25 18:25:51 by hamezoua         ###   ########.fr       */
+/*   Updated: 2026/06/26 20:59:17 by hamezoua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,40 @@ void    take_dongles(t_coder *coder, t_config *config)
     else
         priority = coder->config->time_to_burnout + coder->last_compile_start;
 
-    pthread_mutex_lock(&coder->left_dongle->mutex);
-    pthread_mutex_lock(&coder->right_dongle->mutex);
+    if (coder->left_dongle->id_of_dongle < coder->right_dongle->id_of_dongle)
+    {
+        
+        pthread_mutex_lock(&coder->left_dongle->mutex);
+        pthread_mutex_lock(&coder->right_dongle->mutex);
+    }
+    else
+    {
+        pthread_mutex_lock(&coder->right_dongle->mutex);
+        pthread_mutex_lock(&coder->left_dongle->mutex);
+    }
     heap_insert(coder->left_dongle, coder->id_of_coder, priority);
     heap_insert(coder->right_dongle, coder->id_of_coder, priority);
+
+    while (coder->left_dongle->heap[0].coder_id != coder->id_of_coder || coder->right_dongle->heap[0].coder_id != coder->id_of_coder)
+    {
+        if (coder->left_dongle->heap[0].coder_id != coder->id_of_coder)
+        {
+            pthread_mutex_unlock(&coder->right_dongle->mutex);
+            pthread_cond_wait(&coder->left_dongle->cond, &coder->left_dongle->mutex);
+            pthread_mutex_lock(&coder->right_dongle->mutex);
+        }
+        else if (coder->right_dongle->heap[0].coder_id != coder->id_of_coder)
+        {
+            pthread_mutex_unlock(&coder->left_dongle->mutex);
+            pthread_cond_wait(&coder->right_dongle->cond, &coder->right_dongle->mutex);
+            pthread_mutex_lock(&coder->left_dongle->mutex);
+        }
+        
+    }
+    heap_extract_min(coder->left_dongle);
+    heap_extract_min(coder->right_dongle);
+    pthread_mutex_unlock(&coder->left_dongle->mutex);
+    pthread_mutex_unlock(&coder->right_dongle->mutex);
 }
 void *coder_routine(void *arg)
 {
@@ -43,4 +73,3 @@ void *coder_routine(void *arg)
     }
     
 }
-3277
